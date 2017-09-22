ae219db15aa48387d1918a5a497f39b1a92f7d9a
MPerikhanyan <31830241+MPerikhanyan@users.noreply.github.com>
2017-09-22 21:55:52 +0400

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -23,7 +23,7 @@ const volume = function(length, width, height){
 
   
  const compare = function (num1 , num2){
-    if (num1= num2) {return 0 ;}
+    if (num1 === num2) {return 0 ;}
     else if (num1 > num2)  {return 1;}
     else if (num1 < num2){ return -1 ;}
  };
